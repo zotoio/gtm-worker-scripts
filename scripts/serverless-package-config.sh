8142daf6e40e8e33e22732d0819b40bd809c5a8d
wyvern8 <wyvern@smartcreations.com.au>
2018-07-14 13:52:29 +1000

    setup env

--- scripts/serverless-package-config.sh
+++ scripts/serverless-package-config.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
+
 # collect config and write to files/env
 PACKAGE=$1
 STAGE=$2
 echo "Collecting package configuration for $PACKAGE ($STAGE).."
 
-aws ssm get-parameters-by-path --with-decryption --path /serverless/$PACKAGE/$STAGE
-
 # aws ssm put-parameter --name "/serverless/sample-http/dev/SAMPLE_VAR" --type "String" --value "some value"
+# aws ssm get-parameters-by-path --with-decryption --path /serverless/$PACKAGE/$STAGE
+
+touch /usr/workspace/clone/output/$PACKAGE/set-env-$STAGE.sh
+aws ssm get-parameters-by-path --with-decryption  --path /serverless/${PACKAGE}/${STAGE} \
+| jq -r '.Parameters| .[] | "export " + .Name + "=\"" + .Value + "\""  ' \
+| sed -e "s~/serverless/${PACKAGE}/${STAGE}/~~" >> /usr/workspace/clone/output/$PACKAGE/set-env-$STAGE.sh
+
+cat /usr/workspace/clone/output/$PACKAGE/set-env-$STAGE.sh
+
+source /usr/workspace/clone/output/$PACKAGE/set-env-$STAGE.sh
+
