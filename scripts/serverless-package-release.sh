066134de14267b4e9a333b34ba306cbd9c3fe82a
wyvern8 <wyvern@smartcreations.com.au>
2018-07-14 15:28:21 +1000

    setup env

--- scripts/serverless-package-release.sh
+++ scripts/serverless-package-release.sh
@@ -8,8 +8,11 @@ cd /usr/workspace/clone/packages/$PACKAGE
 source ./serverless-package-config.sh $PACKAGE "prod"
 
 if grep -q "sls-package" ./package.json; then
+    echo "found npm script for packaging release.."
     yarn sls-package >> ${OUT_FILE} 2>&1 || echo "failed $PACKAGE..";
-    https_proxy=$AWS_S3_PROXY aws s3 sync ../../output/packages/$PACKAGE-*.tar.gz s3://$S3_DEPENDENCY_BUCKET/releases/$PACKAGE 2>&1 || echo "failed uploading $PACKAGE..";
+    echo "release package created, uploading to s3://$S3_DEPENDENCY_BUCKET/releases/$PACKAGE"
+    https_proxy=$AWS_S3_PROXY find /usr/workspace/clone/output/packages -name "$PACKAGE-*" | xargs -I {} aws s3 cp {}  s3://$S3_DEPENDENCY_BUCKET/releases/$PACKAGE 2>&1 || echo "failed uploading $PACKAGE..";
+    echo "release upload completed!"
 else
     echo "npm script for packaging $PACKAGE is missing" >> ${OUT_FILE}
 fi
